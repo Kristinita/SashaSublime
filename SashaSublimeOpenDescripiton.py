3cc9db8ae92131ad3d94aeba6f7c9ff95ead733d
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-02-22 16:42:37 +0300

    [Add] Open description command in menu :crystal_ball:

--- SashaSublimeOpenDescripiton.py
+++ SashaSublimeOpenDescripiton.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import sublime, sublime_plugin, webbrowser
 
-class sasha_sublime_description_open(sublime_plugin.TextCommand):
+class sasha_sublime_open_description(sublime_plugin.TextCommand):
     def run(self, edit):
         webbrowser.open_new_tab('http://Kristinita.ru/Sublime-Text/SashaSublime')
